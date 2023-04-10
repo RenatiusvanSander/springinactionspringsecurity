836d341dc24a3070273ce92378d1ff992e11d670
RenatiusvanSander / R. Meier <remad@web.de>
2023-04-10 21:56:22 +0200

    master: initial commit

---
+++ src/main/java/edu/remad/springinactionspringsecurity/SpringinactionspringsecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.remad.springinactionspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinactionspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinactionspringsecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/remad/springinactionspringsecurity/SpringinactionspringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.remad.springinactionspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinactionspringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
